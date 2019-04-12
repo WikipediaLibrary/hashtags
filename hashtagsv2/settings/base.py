33a16a64845a45aca7a428c4d12739ff07d1e3e4
Parth Shandilya <parth1989shandilya@gmail.com>
2019-04-13 01:23:57 +0530

    Added Nose for Coverage

--- hashtagsv2/settings/base.py
+++ hashtagsv2/settings/base.py
@@ -20,6 +20,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hashtagsv2.hashtags',
+    'django_nose',
+]
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=./',
 ]
 
 MIDDLEWARE = [
