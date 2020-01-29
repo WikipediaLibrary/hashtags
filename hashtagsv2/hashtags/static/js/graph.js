1e673d18818063e2ed39f0999ee5136a6a75c4d1
sanyam <sanyam.inbox@gmail.com>
2020-01-29 10:31:10 +0530

    Update graph.js

--- hashtagsv2/hashtags/static/js/graph.js
+++ hashtagsv2/hashtags/static/js/graph.js
@@ -118,47 +118,109 @@ else{
     })
 }
 
+// calling the edits_over_time API when page loads gives default view_type
 $.ajax({
     url: '/api/time_stats/?' + url_param,
     dataType: 'json',
-    success: function(data){
-        var ctx = document.getElementById('timeChart');
-        var timeChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: data['time_array'],
-                datasets: [{
-                    label: 'Number of edits',
-                    data: data['edits_array'],
-                    backgroundColor: 'rgba(255, 99, 132, 0.2)',    
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    borderWidth: 1,
-                    fill: false
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true,
-                            callback: function (value) { if (Number.isInteger(value)) { return value; } }
-                        }
-                    }]
-                },
-                maintainAspectRatio: false,
-                // When chart is rendered, change download and href atribute of anchor 'Download JPG' to allow users to download charts as image
-                // toDataURL function gets url of chart in type of jpeg image
-                animation: { 
-                    onComplete: function(){ 
-                        let time_link = document.getElementById("timeChart").toDataURL("image/jpeg"); 
-                        document.getElementById("time_url").href = time_link; 
-                        document.getElementById("time_url").setAttribute('download', 'edits_over_time.jpg'); 
-                    } 
-                }
-            }
-        });
+    success: function(data) {
+        show_chart(data['view_type']);
+        draw_chart(data);
     },
-    complete: function(){
+    complete: function() {
         $('#loader3').hide();
     }
 })
+
+
+// when user clicks on button to change default view_type
+$(".view-type").click(function() {
+    var chart_name = $(this).attr("name");
+    var status = $('#' + chart_name).attr("status");
+
+    // if chart is loaded previously
+    if (status == "loaded") {
+        show_chart(chart_name);
+    } else {
+        var view_type = $(this).attr("value");
+        //adding view_type parameter to url
+        extra_url_param = url_param + '&amp;view_type=' + view_type
+
+        $.ajax({
+            url: '/api/time_stats/?' + extra_url_param,
+            dataType: 'json',
+            success: function(data) {
+                show_chart(data['view_type']);
+                draw_chart(data);
+            },
+            complete: function() {
+                $('#loader3').hide();
+            }
+        })
+    }
+});
+
+// shows the required chart and hides oher
+function show_chart(view_type) {
+    $(".timeChart").hide();
+    $('#' + view_type).show();
+
+    for (var i = 0; i < 3; i++) {
+        ($(".view-type")[i]).style.borderStyle = "hidden";
+    }
+
+    document.getElementsByName(view_type)[0].style.borderStyle = "solid";
+
+    var status = $('#' + view_type).attr("status");
+    if (status == 'loaded') {
+        let time_link = document.getElementById(view_type).toDataURL("image/jpeg");
+        document.getElementById("time_url").href = time_link;
+        document.getElementById("time_url").setAttribute('download', 'edits_over_time.jpg');
+    } else {
+        $('#' + view_type).attr("status", "loaded");
+    }
+}
+
+// draw the chart from response data
+function draw_chart(data) {
+
+    var ctx = document.getElementById(data['view_type']);
+
+    var timeChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data['time_array'],
+            datasets: [{
+                label: 'Number of edits',
+                data: data['edits_array'],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1,
+                fill: false
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        callback: function(value) {
+                            if (Number.isInteger(value)) {
+                                return value;
+                            }
+                        }
+                    }
+                }]
+            },
+            maintainAspectRatio: false,
+            // When chart is rendered, change download and href atribute of anchor 'Download JPG' to allow users to download charts as image
+            // toDataURL function gets url of chart in type of jpeg image
+            animation: {
+                onComplete: function() {
+                    let time_link = document.getElementById(data['view_type']).toDataURL("image/jpeg");
+                    document.getElementById("time_url").href = time_link;
+                    document.getElementById("time_url").setAttribute('download', 'edits_over_time.jpg');
+                }
+            }
+        }
+    });
+}
